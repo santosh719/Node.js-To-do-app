0ae09f3bd2df8ed3978384031e9a056235505e35
Santosh Bidve <santoshbidave2@gmail.com>
2017-04-10 23:05:52 -0700

    updates

---
+++ app.js
@@ -0,0 +1,19 @@
+//import required modules
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+//initiliaze express
+var app = express();
+
+//setting up template engine
+app.set('view engine','ejs');
+
+//initiliaze static files
+app.use(express.static('./public'));
+
+//invoke controllers
+todoController(app);
+
+//start listening to port
+app.listen(3000);
+console.log("server started on port 3000");
